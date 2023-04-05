ab16592e1aeafcc87dfbd4e71ecfd6cf6f540cd4
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 19:40:08 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -1,31 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.12;
 
-import "./IToken.sol";
+interface IToken {
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
 
 contract CryptoAI {
-    IToken public token;
+    IToken private token;
+    mapping(address => uint256) private balances;
 
-    mapping(address => uint256) public balances;
-
-    constructor(address tokenAddress) {
-        token = IToken(tokenAddress);
+    constructor(address _token) {
+        token = IToken(_token);
     }
 
     function deposit(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
+        require(amount > 0, "Deposit amount must be greater than zero");
         require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
         balances[msg.sender] += amount;
     }
 
     function withdraw(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
+        require(amount > 0, "Withdrawal amount must be greater than zero");
         require(balances[msg.sender] >= amount, "Insufficient balance");
         balances[msg.sender] -= amount;
         require(token.transfer(msg.sender, amount), "Transfer failed");
     }
 
-    function getBalance() external view returns (uint256) {
-        return balances[msg.sender];
+    function getBalance(address account) external view returns (uint256) {
+        return balances[account];
     }
 }
+
