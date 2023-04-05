418fe34882657364988e1b291f70590cca925d88
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 19:04:58 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -2,39 +2,29 @@
 
 pragma solidity ^0.8.0;
 
-interface IToken {
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
+import "./IToken.sol";
 
 contract CryptoAI {
-    IToken private token;
-    mapping(address => uint256) private balances;
+    IToken public token;
+    mapping(address => uint256) public balances;
 
-    event Deposit(address indexed account, uint256 amount);
-    event Withdrawal(address indexed account, uint256 amount);
+    event Deposit(address indexed depositor, uint256 amount);
+    event Withdrawal(address indexed depositor, uint256 amount);
 
-    constructor(address tokenAddress) {
-        token = IToken(tokenAddress);
+    constructor(IToken _token) {
+        token = _token;
     }
 
     function deposit(uint256 amount) external {
-        require(amount > 0, "Deposit amount must be greater than 0");
-        require(token.transferFrom(msg.sender, address(this), amount), "TransferFrom failed");
+        require(amount > 0, "Amount must be greater than zero");
+
+        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+
         balances[msg.sender] += amount;
+
         emit Deposit(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
-        require(amount > 0, "Withdrawal amount must be greater than 0");
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        require(token.transfer(msg.sender, amount), "Transfer failed");
-        emit Withdrawal(msg.sender, amount);
-    }
-
-    function balance() external view returns (uint256) {
-        return balances[msg.sender];
-    }
-}
+        require(amount > 0, "Amount must be greater than zero");
+        require(balances[msg.sender] >= amount, "Ins
