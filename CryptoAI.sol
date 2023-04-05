7e4eee4f7f3c6cdf832f3676de87b7d46ec8eb27
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 18:59:40 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -1,31 +1,40 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 interface IToken {
-    function transfer(address recipient, uint256 amount) external view returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
 }
 
 contract CryptoAI {
-    IToken public token;
-    mapping(address => uint256) public balances;
+    IToken private token;
+    mapping(address => uint256) private balances;
+
+    event Deposit(address indexed account, uint256 amount);
+    event Withdrawal(address indexed account, uint256 amount);
 
     constructor(address tokenAddress) {
         token = IToken(tokenAddress);
     }
 
-    function deposit(uint256 amount) public {
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+    function deposit(uint256 amount) external {
+        require(amount > 0, "Deposit amount must be greater than 0");
+        require(token.transferFrom(msg.sender, address(this), amount), "TransferFrom failed");
         balances[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
     }
 
-    function withdraw(uint256 amount) public {
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Withdrawal amount must be greater than 0");
         require(balances[msg.sender] >= amount, "Insufficient balance");
-        require(token.transfer(msg.sender, amount), "Transfer failed");
         balances[msg.sender] -= amount;
+        require(token.transfer(msg.sender, amount), "Transfer failed");
+        emit Withdrawal(msg.sender, amount);
     }
 
-    function getBalance() public view returns (uint256) {
-        return token.balanceOf(address(this));
+    function balance() external view returns (uint256) {
+        return balances[msg.sender];
     }
 }
