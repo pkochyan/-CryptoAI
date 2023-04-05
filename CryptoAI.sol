741542a9877cb0121ecbfb52601ff2b6476a892a
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 18:02:02 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -1,24 +1,27 @@
-// SPDX-License-Identifier: GPL-3.0
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract CryptoAI {
     mapping(address => uint) balances;
 
-    function deposit() public payable {
-        require(msg.value > 0, "Invalid deposit amount");
+    event Deposit(address indexed account, uint amount);
+    event Withdrawal(address indexed account, uint amount);
+
+    function deposit() external payable {
+        require(msg.value > 0, "Amount should be greater than 0");
         balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint amount) public {
-        require(amount > 0, "Invalid withdrawal amount");
-        require(balances[msg.sender] >= amount, "Insufficient balance");
+    function withdraw(uint amount) external {
+        require(amount > 0, "Amount should be greater than 0");
+        require(amount <= balances[msg.sender], "Insufficient balance");
+        payable(msg.sender).transfer(amount);
         balances[msg.sender] -= amount;
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require(success, "Withdrawal failed");
+        emit Withdrawal(msg.sender, amount);
     }
 
-    function getBalance() public view returns (uint) {
-        return balances[msg.sender];
+    function getBalance(address account) external view returns (uint) {
+        return balances[account];
     }
 }
