aa88c969123e1b7ed74fec6b8dea038bbafe5a1a
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 18:39:26 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -1,38 +1,33 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IVirtualAsset {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external view returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external view returns (bool);
     function balanceOf(address account) external view returns (uint256);
 }
 
 contract CryptoAI {
     IVirtualAsset public virtualAsset;
-    mapping(address => uint256) public balances;
-
-    event Deposited(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
 
     constructor(address _virtualAsset) {
         virtualAsset = IVirtualAsset(_virtualAsset);
     }
 
     function deposit(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(virtualAsset.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(virtualAsset.balanceOf(msg.sender) >= amount, "Insufficient balance");
 
-        balances[msg.sender] += amount;
-        emit Deposited(msg.sender, amount);
+        bool success = virtualAsset.transferFrom(msg.sender, address(this), amount);
+        require(success, "Transfer failed");
     }
 
     function withdraw(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(balances[msg.sender] >= amount, "Insufficient balance");
+        require(virtualAsset.balanceOf(address(this)) >= amount, "Insufficient balance");
 
-        balances[msg.sender] -= amount;
-        require(virtualAsset.transfer(msg.sender, amount), "Transfer failed");
+        bool success = virtualAsset.transfer(msg.sender, amount);
+        require(success, "Transfer failed");
+    }
 
-        emit Withdrawn(msg.sender, amount);
+    function getBalance() external view returns (uint256) {
+        return virtualAsset.balanceOf(address(this));
     }
 }
