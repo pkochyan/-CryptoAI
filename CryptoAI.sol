fda74dac8edb0805633c94c23513d45fa5a728ef
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 18:06:21 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IVirtualAsset {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+}
+
 contract CryptoAI {
-    mapping(address => uint) balances;
+    IVirtualAsset public virtualAsset;
 
-    event Deposit(address indexed account, uint amount);
-    event Withdrawal(address indexed account, uint amount);
-
-    function deposit() external payable {
-        require(msg.value > 0, "Amount should be greater than 0");
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
+    constructor(address virtualAssetAddress) {
+        virtualAsset = IVirtualAsset(virtualAssetAddress);
     }
 
-    function withdraw(uint amount) external {
-        require(amount > 0, "Amount should be greater than 0");
-        require(amount <= balances[msg.sender], "Insufficient balance");
-        payable(msg.sender).transfer(amount);
-        balances[msg.sender] -= amount;
-        emit Withdrawal(msg.sender, amount);
+    function deposit(uint256 amount) external {
+        require(amount > 0, "Deposit amount must be greater than 0");
+        virtualAsset.transferFrom(msg.sender, address(this), amount);
     }
 
-    function getBalance(address account) external view returns (uint) {
-        return balances[account];
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Withdrawal amount must be greater than 0");
+        require(virtualAsset.balanceOf(address(this)) >= amount, "Insufficient balance");
+
+        // Ensure that the transfer is performed before the state is updated
+        (bool success, ) = msg.sender.call{value: 0}("");
+        require(success, "Transfer failed");
+
+        virtualAsset.transfer(msg.sender, amount);
     }
 }
