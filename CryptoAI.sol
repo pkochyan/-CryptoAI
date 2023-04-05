30aac9c1882730ac0a199182f862f56932e5919d
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 19:16:24 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -5,38 +5,27 @@ import "./IToken.sol";
 
 contract CryptoAI {
     IToken public token;
+
     mapping(address => uint256) public balances;
 
-    event Deposit(address indexed depositor, uint256 amount);
-    event Withdrawal(address indexed depositor, uint256 amount);
-
-    constructor(IToken _token) {
-        token = _token;
+    constructor(address tokenAddress) {
+        token = IToken(tokenAddress);
     }
 
     function deposit(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
+        require(amount > 0, "Amount must be greater than 0");
         require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
-
         balances[msg.sender] += amount;
-
-        emit Deposit(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
+        require(amount > 0, "Amount must be greater than 0");
         require(balances[msg.sender] >= amount, "Insufficient balance");
-
         balances[msg.sender] -= amount;
-
         require(token.transfer(msg.sender, amount), "Transfer failed");
-
-        emit Withdrawal(msg.sender, amount);
     }
 
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+    function getBalance() external view returns (uint256) {
+        return balances[msg.sender];
     }
 }
-
