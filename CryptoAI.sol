74955f97a7f119e649dc05478ac8a628c0a9d87e
pkochyan <129990976+pkochyan@users.noreply.github.com>
2023-04-05 20:13:28 -0400

    Update CryptoAI.sol

--- CryptoAI.sol
+++ CryptoAI.sol
@@ -1,35 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity ^0.8.0;
 
-interface IToken {
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CryptoAI {
-    IToken private token;
-    mapping(address => uint256) private balances;
+contract CryptoAI is ERC20 {
+    address public owner;
+    uint256 public constant MAX_SUPPLY = 1000000000 * 10**18; // 1 billion tokens
+    uint256 public constant INITIAL_SUPPLY = 1000000 * 10**18; // 1 million tokens
 
-    constructor(address _token) {
-        token = IToken(_token);
+    constructor() ERC20("CryptoAI", "CAI") {
+        owner = msg.sender;
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 
-    function deposit(uint256 amount) external {
-        require(amount > 0, "Deposit amount must be greater than zero");
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
-        balances[msg.sender] += amount;
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == owner, "Only the owner can mint tokens");
+        require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds maximum supply");
+        _mint(to, amount);
     }
 
-    function withdraw(uint256 amount) external {
-        require(amount > 0, "Withdrawal amount must be greater than zero");
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        require(token.transfer(msg.sender, amount), "Transfer failed");
-    }
-
-    function getBalance(address account) external view returns (uint256) {
-        return balances[account];
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
-
